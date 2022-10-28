6f05015869ebd2162c1e171bb6dfbb62f6449005
rvanasa <ryan.vandersmith@dfinity.org>
2022-10-28 11:30:22 -0600

    Reformat

--- src/routes/canister.ts
+++ src/routes/canister.ts
@@ -1,30 +1,26 @@
 import express from 'express';
 import wasm from '../wasm';
-import {json}from 'body-parser'
+import { json } from 'body-parser';
 
 export default (app: express.Application) => {
-    app.post(
-        '/alias/:alias([^/]+)/:method',
-        json(),
-        (req, res, next) => {
-            try {
-                const { alias, method } = req.params;
-                const message = req.body;
+    app.post('/alias/:alias([^/]+)/:method', json(), (req, res, next) => {
+        try {
+            const { alias, method } = req.params;
+            const message = req.body;
 
-                console.log(alias, message);
+            console.log(alias, message);
 
-                const result = wasm.handle_message(alias, method, message);
+            const result = wasm.handle_message(alias, method, message);
 
-                console.log('RESULT:', result); ///
+            console.log('RESULT:', result); ///
 
-                // res.status(500).send('Unimplemented');
-                res.status(200).end();
-                // res.status(200).send(cborEncode({}));
-            } catch (err) {
-                next(err);
-            }
-        },
-    );
+            // res.status(500).send('Unimplemented');
+            res.status(200).end();
+            // res.status(200).send(cborEncode({}));
+        } catch (err) {
+            next(err);
+        }
+    });
 };
 
 // export default router;
