55e2b1d2f01f6c998ff62b9b56c5a037ebec0022
rvanasa <ryan.vandersmith@dfinity.org>
2023-01-10 17:55:51 -0700

    Refactor unit tests

--- tests/devServer.test.ts
+++ tests/devServer.test.ts
@@ -4,23 +4,15 @@ import { join } from 'path';
 import devServer from '../src';
 
 const projectPath = join(__dirname, 'project');
-const outputPath = join(projectPath, 'output');
 
 const waitUntilLoaded = async () => {
     await new Promise((resolve) => setTimeout(resolve, 1000)); // TODO: deterministic
 };
 
 describe('mo-dev', () => {
-    beforeEach(() => {
-        if (existsSync(outputPath)) {
-            unlinkSync(outputPath);
-        }
-    });
-
     test('detects Motoko files', async () => {
         const { watcher, close } = devServer({
             directory: projectPath,
-            execute: `echo "ran command" >> output`,
         });
 
         const files: string[] = [];
@@ -36,9 +28,9 @@ describe('mo-dev', () => {
             files.sort();
             expect(files).toStrictEqual([
                 'dfx.json',
-                'replica_canister/Main.mo',
-                'replica_canister/lib/Echo.mo',
-                'vm_canister/Main.mo',
+                'motoko_canister/Main.mo',
+                'motoko_canister/lib/Echo.mo',
+                'vm/vm_canister/Main.mo',
             ]);
         } finally {
             close();
@@ -46,9 +38,14 @@ describe('mo-dev', () => {
     });
 
     test('runs a provided command', async () => {
+        const outputPath = join(projectPath, 'generated.txt');
+        if (existsSync(outputPath)) {
+            unlinkSync(outputPath);
+        }
+
         const { close } = devServer({
             directory: projectPath,
-            execute: `echo "ran command" >> output`,
+            execute: `echo "ran command" >> generated.txt`,
         });
 
         await waitUntilLoaded();
@@ -62,7 +59,7 @@ describe('mo-dev', () => {
     test('starts the VM server on a custom port', async () => {
         const port = 56789;
         const { close } = devServer({
-            directory: projectPath,
+            directory: join(projectPath, 'vm'),
             hotReload: true,
             port,
         });
@@ -70,9 +67,7 @@ describe('mo-dev', () => {
         try {
             const response = await axios.post(
                 `http://localhost:${port}/call/vm_canister/main`,
-                {
-                    args: [],
-                },
+                { args: [] },
             );
             expect(response.data).toStrictEqual({ value: '123' });
         } finally {
