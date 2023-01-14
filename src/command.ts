9974bf6021374deb4e02ebdc438641457e27bec8
rvanasa <ryan.vandersmith@dfinity.org>
2023-01-14 15:14:59 -0700

    Fix 'cwd' command

--- src/command.ts
+++ src/command.ts
@@ -12,50 +12,45 @@ const examples: [string, string][] = [
     ['-g', 'generate TypeScript bindings on file change'],
 ];
 
-const {
-    directory,
-    version,
-    port,
-    delay,
-    exec,
-    generate,
-    deploy,
-    yes,
-    hotReload,
-} = program
-    .name('mo-dev')
-    .description(
-        `Examples:\n${examples
-            .map(
-                ([usage, description]) =>
-                    `  $ mo-dev ${usage}  # ${description}`,
-            )
-            .join('\n')}`,
-    )
-    .option('-V, --version', `show installed version`)
-    .option('-c, --cwd <directory>', 'directory containing a `dfx.json` file')
-    .option('-d, --deploy', `run \`dfx deploy\` on changed canister`)
-    .option(
-        '-y, --yes',
-        `respond "yes" to reinstall prompts (may reset canister state)`,
-    )
-    .option('-g, --generate', `run \`dfx generate\` on changed canister`)
-    .option('-x, --exec <exec>', `execute command on file change`)
-    .option('-v, --verbose', `show more details in console`, increaseVerbosity)
-    .addOption(
-        new Option(
-            '--hot-reload',
-            `hot module replacement server (experimental)`,
-        ).hideHelp(),
-    )
-    .addOption(
-        new Option(
-            '-p, --port <port>',
-            `hot module replacement server port (default: ${defaultSettings.port})`,
-        ).hideHelp(),
-    )
-    .parse()
-    .opts();
+const { cwd, version, port, delay, exec, generate, deploy, yes, hotReload } =
+    program
+        .name('mo-dev')
+        .description(
+            `Examples:\n${examples
+                .map(
+                    ([usage, description]) =>
+                        `  $ mo-dev ${usage}  # ${description}`,
+                )
+                .join('\n')}`,
+        )
+        .option('-V, --version', `show installed version`)
+        .option('-c, --cwd <cwd>', 'directory containing a `dfx.json` file')
+        .option('-d, --deploy', `run \`dfx deploy\` on changed canister`)
+        .option(
+            '-y, --yes',
+            `respond "yes" to reinstall prompts (may reset canister state)`,
+        )
+        .option('-g, --generate', `run \`dfx generate\` on changed canister`)
+        .option('-x, --exec <exec>', `execute command on file change`)
+        .option(
+            '-v, --verbose',
+            `show more details in console`,
+            increaseVerbosity,
+        )
+        .addOption(
+            new Option(
+                '--hot-reload',
+                `hot module replacement server (experimental)`,
+            ).hideHelp(),
+        )
+        .addOption(
+            new Option(
+                '-p, --port <port>',
+                `hot module replacement server port (default: ${defaultSettings.port})`,
+            ).hideHelp(),
+        )
+        .parse()
+        .opts();
 
 if (version) {
     console.log('mo-dev', require('../package.json').version);
@@ -63,7 +58,7 @@ if (version) {
 }
 
 const settings: Settings = {
-    directory: resolve(process.cwd(), directory || '.'),
+    directory: resolve(cwd || '.'),
     port: port ? +port : defaultSettings.port,
     delay: !!delay || defaultSettings.delay,
     execute: exec || defaultSettings.execute,
