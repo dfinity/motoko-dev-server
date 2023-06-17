d21fa0aff87959c45061c6dc0f041b001355f1e4
rvanasa <ryan.vandersmith@dfinity.org>
2023-06-17 10:39:12 -0600

    Re-enable Wasm bindings

--- src/wasm.ts
+++ src/wasm.ts
@@ -1,19 +1,3 @@
-// import wasm from '../wasm/pkg/nodejs/wasm';
-// export default wasm;
+import wasm from '../wasm/pkg/nodejs/wasm';
 
-// Wasm functionality is currently unused
-export default {
-    update_settings(settings: any) {},
-    update_canister(path: string, alias: string, source: string) {
-        throw new Error('Unsupported runtime');
-    },
-    remove_canister(alias: string) {
-        throw new Error('Unsupported runtime');
-    },
-    call_canister(...args: any[]) {
-        throw new Error('Unsupported runtime');
-    },
-    candid_to_js(candid: string) {
-        throw new Error('Unsupported runtime');
-    },
-};
+export default wasm;
